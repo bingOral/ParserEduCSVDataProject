4d2826fd0741dc0007a972a33bcd4597dbd73c4a
Yankaituo <yankaituo@gmail.com>
2019-01-02 01:21:55 -0500

    test

--- script/DownloadEduOpusData.pm
+++ script/DownloadEduOpusData.pm
@@ -3,35 +3,37 @@
 use strict;
 package EduTool;
 
-use warnings;
-use LWP::UserAgent;
 use JSON;
+use LWP::UserAgent;
 use POSIX qw(strftime);
+use Search::Elasticsearch;
 
 sub dowork
 {
 	my $param = shift;
 	my $jsonparser = new JSON;
+	my $es = Search::Elasticsearch->new(nodes=>['192.168.1.20:9200'], cxn_pool => 'Sniff');
 
-	my $json = $jsonparser->decode($param);
-	my $url = $json->{url};
-	my $text = $json->{sample};
-
-	my $filename = create($url);
-	open(OUT,">$filename")||die("The file can't create!\n");
-
-	my $ua = LWP::UserAgent->new;
-	$ua->default_header('format' => "opus");
-	 
-	my $response = $ua->get($url);
-	if($response->is_success)
+	open(IN,$param)||die("The file can't find!\n");
+	while(my $row = <IN>)
 	{
-		print $filename."|".$text."\n";
-		print OUT $response->decoded_content;
-	}
-	else
-	{
-	    print $url."\n";
+		chomp($row);
+		my $json = $jsonparser->decode($row);
+		my $url = $json->{url};
+		my $text = $json->{sample};
+		my $score = $json->{score};
+		my $length = $json->{length};
+
+		if($score >= 95)
+		{
+			my $filename = create($url);
+			my $code = getstore($url,$filename);
+			if($code == 200)
+			{
+				insertElastic($es,$filename,$url,$text,$length,$score);
+			}
+			#die;
+		}
 	}
 }
 
@@ -51,6 +53,42 @@ sub create
 	`mkdir -p '/backup/edu-data/'$res`;
 
 	return '/backup/edu-data/'.$res.'/'.$filename.'.opus';
+}
+
+sub getstore
+{
+	my($url, $file) = @_;
+	my $ua = LWP::UserAgent->new;
+	$ua->default_header('format' => 'opus');
+	$ua->agent('Mozilla/5.0 '.$ua->_agent);
+
+	my $request = HTTP::Request->new(GET => $url);
+	my $response = $ua->request($request, $file);
+ 
+	$response->code;
+}
+
+sub insertElastic
+{
+	my $es = shift;
+	my $filename = shift;
+	my $url = shift;
+	my $sample = shift;
+	my $length = shift;
+	my $score = shift;
+	
+	$es->index(
+		index => 'callserv_edu_oral_en',
+		type  => 'data',
+		id    => $filename,
+		body  => {
+			wavname => $filename,
+			text    => $sample,
+			server  => $url,
+			length  => $length,	
+		     reference  => $score
+		}
+	);
 }
 
 1;
